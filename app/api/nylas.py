ff7c362a278c7eb3d2a31bd2a42f571f9bf18ca2
Subash Pradhan <subashpradhan@mac.home>
2024-10-08 21:43:36 +0200

    Generate JWT token on successful authentication

--- app/api/nylas.py
+++ app/api/nylas.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, make_response
 from app.utils.adaptor_utils import get_nylas_client
 from app.utils.response_utils import error_response, success_response
+from app.utils.jwt_utils import generate_jwt_token
 from flask import request
+import datetime
 import logging
+
+
 log = logging.getLogger(__name__)
 
 nylas = Blueprint("nylas", __name__)
@@ -13,7 +17,8 @@ def register_mailbox():
   try:
     nylas_adaptor = get_nylas_client()
     authentication_request = nylas_adaptor.generate_authentication_url()
-    return redirect(authentication_request)
+    authentication_url = {"redirect_url": authentication_request}
+    return success_response(authentication_url)
   except Exception as e:
     return error_response("An error occurred while generating authentication url, please try again", status_code=503, errors=str(e))
   
@@ -25,8 +30,17 @@ def callback_uri():
     code = request.args.get("code")
     nylas_adaptor = get_nylas_client()
     response = nylas_adaptor.exchange_code_for_token(code)
-    log.info("Authentication successfull")
-    return success_response(response)  # TODO: this should redirect to frontend, we should have way to store the grant ID.
+    grant_id = response.get("grant_id")
+    payload = {
+      "grant_id": grant_id,
+      "exp": datetime.datetime.now() + datetime.timedelta(hours=1)
+    }
+    token = generate_jwt_token(payload)
+    frontend_url = "http://localhost:3000/nylas-demo"
+    resp = make_response(redirect(frontend_url))
+    resp.set_cookie("token", token, httponly=True, secure=False)
+    resp.set_cookie("grant_id", grant_id, httponly=True, secure=False)
+    return resp
   except Exception as e:
     log.error("Authentication failed during code exchange", str(e))
     return error_response("Authentication failed", status_code=503, errors=str(e))
--- app/config.py
+++ app/config.py
@@ -9,4 +9,4 @@ class BaseConfig:
   NYLAS_API_KEY = os.environ.get("NYLAS_API_KEY")
   NYLAS_REDIRECT_URI = "http://localhost:8000/nylas/callback"
   NYLAS_BASE_URL = "https://api.us.nylas.com/v3"
-
+  JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
